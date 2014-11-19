d0475c1f10411dd63de7389645d5b83240a20d7c
finde <finde@ilumy.com>
2014-11-19 14:48:48 +0100

    Q-Learning + softmax

--- app/scripts/brain/single_agent_learning/qlearning.js
+++ app/scripts/brain/single_agent_learning/qlearning.js
@@ -6,31 +6,37 @@
  * @param epsilon = action selection variable
  * @constructor
  */
-var QLearning = function (nLearning, stateSpace, world, gamma, alpha, epsilon) {
+var QLearning = function (options) {
+
+  if (!options) {
+    options = {};
+  }
 
   // properties
-  if (!world) {
-    world = new World();
-    world.setSize(11);
-    world.isLogEnabled = false;
-  }
+  var world = new World();
+  world.setSize(11);
+  world.isLogEnabled = false;
 
   var worldSize = world.getSize();
 
-  if (!gamma) {
-    gamma = 0.8; // 0.1, 0.5, 0.7, 0.9
+  if (!options.gamma) {
+    options.gamma = 0.8; // 0.1, 0.5, 0.7, 0.9
   }
 
-  if (!alpha) {
-    alpha = 0.5; // 0.1 ... 0.5
+  if (!options.alpha) {
+    options.alpha = 0.5; // 0.1 ... 0.5
   }
 
-  if (!epsilon) {
-    epsilon = 0.1;
+  if (!options.epsilon) {
+    options.epsilon = 0.1;
   }
 
-  if (!nLearning) {
-    nLearning = 1000;
+  if (!options.nLearning) {
+    options.nLearning = 100;
+  }
+
+  if (!options.actionSelector) {
+    options.actionSelector = 'softmax';
   }
 
   var predatorActions = world.getPredatorActions();
@@ -41,34 +47,69 @@ var QLearning = function (nLearning, stateSpace, world, gamma, alpha, epsilon) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
   };
 
-  var greedyActionSelections = function (epsilon, currentStateIndex, stateSpace) {
-    var random = Math.random();
+  var actionSelection = function (actionSelector, opts) {
+    var softmaxActionSelection = function (currentStateIndex, stateSpace) {
 
-    var action; // action Object
-    if (random < epsilon) {
-      // choose random action
-      action = predatorActions[rand(0, _.size(predatorActions) - 1)];
-    } else {
-      // choose argmax q(s,a)
       var actionValues = _.pluck(stateSpace[currentStateIndex].actionValues, 'value');
-      var maxValue = numbers.basic.max(actionValues);
 
-      for (var i = 0; i < actionValues.length; i++) {
-        if (actionValues[i] == maxValue) {
-          action = stateSpace[currentStateIndex].actionValues[i];
-          break;
+      var expValues = [];
+      _.each(actionValues, function (actionValue) {
+        expValues.push(Math.exp(actionValue));
+      });
+
+      var randomValue = Math.random() * numbers.basic.sum(expValues);
+
+      for (var i = 0; i < _.size(actionValues); i++) {
+        if (randomValue <= expValues[i]) {
+          return stateSpace[currentStateIndex].actionValues[i];
+        } else {
+          randomValue -= expValues[i];
         }
       }
-    }
 
-    return action;
+      // to ensure it return something
+      return _.first(stateSpace[currentStateIndex].actionValues);
+    };
+
+    var greedyActionSelections = function (epsilon, currentStateIndex, stateSpace) {
+      var random = Math.random();
+
+      var action; // action Object
+      if (random < epsilon) {
+        // choose random action
+        action = predatorActions[rand(0, _.size(predatorActions) - 1)];
+      } else {
+        // choose argmax q(s,a)
+        var actionValues = _.pluck(stateSpace[currentStateIndex].actionValues, 'value');
+        var maxValue = numbers.basic.max(actionValues);
+
+        for (var i = 0; i < actionValues.length; i++) {
+          if (actionValues[i] == maxValue) {
+            action = stateSpace[currentStateIndex].actionValues[i];
+            break;
+          }
+        }
+      }
+
+      return action;
+    };
+
+    switch (actionSelector) {
+      case 'greedy':
+        console.log('greedy');
+        return greedyActionSelections(opts.epsilon, opts.currentStateIndex, opts.stateSpace);
+
+      case 'softmax':
+        console.log('softmax');
+        return softmaxActionSelection(opts.currentStateIndex, opts.stateSpace);
+    }
   };
 
   // Algorithm
   // - init Q(s,a) = 15
-  if (!stateSpace) {
-    stateSpace = new OptimizedStateSpace(worldSize, 0);
-    _.each(stateSpace, function (state) {
+  if (!options.stateSpace) {
+    options.stateSpace = new OptimizedStateSpace(worldSize, 0);
+    _.each(options.stateSpace, function (state) {
 
       state.actionValues = [];
 
@@ -86,8 +127,8 @@ var QLearning = function (nLearning, stateSpace, world, gamma, alpha, epsilon) {
   }
 
   // for each episode - until n times
-  var a, s, sPrime, r, results = [], trials = [];
-  for (var episode = 0; episode < nLearning; episode++) {
+  var a, s, sPrime, r;
+  for (var episode = 0; episode < options.nLearning; episode++) {
 
     // Init s
     s = encodeRelativeDistance({x: 0, y: 0}, {x: 5, y: 5}, worldSize);
@@ -96,7 +137,12 @@ var QLearning = function (nLearning, stateSpace, world, gamma, alpha, epsilon) {
     var innerLoopStep = 0;
     do {
       // choose a from s using policy derived from Q (e.e e-greedy)
-      a = greedyActionSelections(epsilon, s, stateSpace);
+//      a = greedyActionSelections(options.epsilon, s, options.stateSpace);
+      a = actionSelection(options.actionSelector, {
+        epsilon: options.epsilon,
+        currentStateIndex: s,
+        stateSpace: options.stateSpace
+      });
 
       // take action a, observe r and s'
       var sAfterPredatorAction = transitionFunction(s, 'predator', a, worldSize);
@@ -115,18 +161,18 @@ var QLearning = function (nLearning, stateSpace, world, gamma, alpha, epsilon) {
       }
 
       // update q(s,a)
-      var actionValues = _.pluck(stateSpace[sPrime].actionValues, 'value');
+      var actionValues = _.pluck(options.stateSpace[sPrime].actionValues, 'value');
       var qSPrimeA = numbers.basic.max(actionValues);
 
-      if (!stateSpace[s].actionValues[a.index].exp) {
-        stateSpace[s].actionValues[a.index].exp = 0;
+      if (!options.stateSpace[s].actionValues[a.index].exp) {
+        options.stateSpace[s].actionValues[a.index].exp = 0;
       }
 
-      stateSpace[s].actionValues[a.index].exp++;
-      stateSpace[s].actionValues[a.index].value += alpha * (r + gamma * qSPrimeA - stateSpace[s].actionValues[a.index].value);
+      options.stateSpace[s].actionValues[a.index].exp++;
+      options.stateSpace[s].actionValues[a.index].value += options.alpha * (r + options.gamma * qSPrimeA - options.stateSpace[s].actionValues[a.index].value);
 
       // round precision
-      stateSpace[s].actionValues[a.index].value = Math.round(stateSpace[s].actionValues[a.index].value * 1e6) / 1e6;
+      options.stateSpace[s].actionValues[a.index].value = Math.round(options.stateSpace[s].actionValues[a.index].value * 1e6) / 1e6;
 
       // update s <- s'
       s = sPrime;
@@ -134,18 +180,41 @@ var QLearning = function (nLearning, stateSpace, world, gamma, alpha, epsilon) {
       innerLoopStep++;
     } while (s !== '0_0');
 
-    if (episode % 50 == 0) {
-      results.push(innerLoopStep);
-      trials.push(episode);
+    if (!options.results) {
+      options.results = [];
     }
+    options.results.push(innerLoopStep);
 
-    console.log(episode);
+    console.log('episode:', episode);
   }
 
-//  return results;
+  function averagingData(data, nPoints) {
+    var batchSize = _.size(data) / nPoints;
+    var _results = {
+      labels: [],
+      data: []
+    };
+
+    var i = 0, x = 0;
+    while (i < _.size(data)) {
+
+      var temp = [];
+      for (var j = 0; j < batchSize && i < _.size(data); j++, i++) {
+        temp.push(data[i]);
+      }
+
+      _results.labels.push(++x);
+      _results.data.push(numbers.statistic.mean(temp));
+    }
+
+    return _results;
+  }
+
+  var results = averagingData(options.results, 100);
+  console.log(results);
 
   var data = {
-    labels: trials,
+    labels: results.labels,
     datasets: [
       {
         label: "results",
@@ -155,7 +224,7 @@ var QLearning = function (nLearning, stateSpace, world, gamma, alpha, epsilon) {
         pointStrokeColor: "#fff",
         pointHighlightFill: "#fff",
         pointHighlightStroke: "rgba(220,220,220,1)",
-        data: results
+        data: results.data
       }
     ]
   };
@@ -167,7 +236,7 @@ var QLearning = function (nLearning, stateSpace, world, gamma, alpha, epsilon) {
   scenario.push({predator: {x: 10, y: 10}, prey: {x: 0, y: 0}});
 
   _.each(scenario, function (s) {
-    console.log(stateSpace[encodeRelativeDistance(s.predator, s.prey, worldSize)]);
+    console.log(options.stateSpace[encodeRelativeDistance(s.predator, s.prey, worldSize)]);
   });
 
 
@@ -175,10 +244,11 @@ var QLearning = function (nLearning, stateSpace, world, gamma, alpha, epsilon) {
   var ctx = document.getElementById("chart").getContext("2d");
   var myLineChart = new Chart(ctx).Line(data, {pointDot: false, responsive: true, maintainAspectRatio: false});
 
-  $('#stateSpaceOutput').text(JSON.stringify(stateSpace));
+  $('#stateSpaceOutput').text(JSON.stringify(options));
 };
 
-var reRunQLearning = function (nTimes) {
+var reRunQLearning = function (opts) {
+
   function isValidJson(json) {
     try {
       JSON.parse(json);
@@ -190,10 +260,9 @@ var reRunQLearning = function (nTimes) {
 
   var source = $('#stateSpaceOutput').text();
   if (isValidJson(source)) {
-    var exp = JSON.parse(source);
-    QLearning(nTimes, exp);
-  } else {
-    QLearning(nTimes);
+    opts = JSON.parse(source);
   }
+
+  QLearning(opts);
 
 };
