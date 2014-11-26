95862a12af048627ba0e1043814645ec1caf5666
finde <finde@ilumy.com>
2014-11-26 13:44:03 +0100

    updated helper function

--- app/scripts/brain/helperFunctions.js
+++ app/scripts/brain/helperFunctions.js
@@ -373,3 +373,12 @@ var drawChart = function (results, nbins, stateSpace, worldSize) {
   var myLineChart = new Chart(ctx).Line(data, {pointDot: false, responsive: true, maintainAspectRatio: false});
 
 };
+
+var isOptimalAction = function (initState, endState) {
+  var getDistance = function (state) {
+    var x = state.split('_');
+    return Math.abs(x[0] * 1.0) + Math.abs(x[1] * 1.0);
+  };
+
+  return getDistance(endState) < getDistance(initState);
+};
