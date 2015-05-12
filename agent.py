0a09d52afd74fd109918ea55b5082d0cf4e5e72b
Arif Qodari <arif.qodari@gmail.com>
2015-05-12 14:34:01 +0200

    added toroidal to agent

--- agent.py
+++ agent.py
@@ -10,7 +10,7 @@ class Agent:
         self.action_space = np.round(np.arange(-1.5, 1.5, 0.1), 1)
 
 
-    def move(self, distance=None):
+    def move(self, distance=None, toroidal_function = None):
         """
         action space for agent is discretized
         it can move to any point within a circle with radius 1.5 from its
@@ -21,7 +21,12 @@ class Agent:
             # move randomly
             distance = np.random.choice(self.action_space, 2)
 
-        self.position += distance
+        new_position = self.position + distance
+
+        if toroidal_function:
+            self.position = toroidal_function(new_position)
+        else:
+            self.position = new_position
 
         return self.position
 
