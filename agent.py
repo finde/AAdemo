f0a0be8851f16c93f97121dce9ca44ab364c816d
Arif Qodari <arif.qodari@gmail.com>
2015-05-12 14:52:36 +0200

    move toroidal to init

--- agent.py
+++ agent.py
@@ -7,12 +7,13 @@ class Agent:
     class agent
     """
 
-    def __init__(self, init_position=None):
+    def __init__(self, init_position=None, toroidal_function = None):
         self.position = self.__set_init_position(init_position)
         self.action_space = np.round(np.arange(-1.5, 1.5, 0.1), 1)
+        self.toroidal_function = toroidal_function
 
 
-    def move(self, distance=None, toroidal_function=None):
+    def move(self, distance=None):
         """
         action space for agent is discretized
         it can move to any point within a circle with radius 1.5 from its
@@ -23,8 +24,8 @@ class Agent:
             # move randomly
             distance = np.random.choice(self.action_space, 2)
 
-        if toroidal_function:
-            self.position = toroidal_function(self.position + distance)
+        if self.toroidal_function:
+            self.position = self.toroidal_function(self.position + distance)
         else:
             self.position += distance
 
@@ -38,16 +39,17 @@ class Agent:
 
 
 class Prey(Agent):
-    def __init__(self, init_position=None, cov=None):
+    def __init__(self, init_position=None, cov=None, toroidal_function=None):
         if not cov:
             cov = [[1, 0], [0, 1]]
         Agent.__init__(self, init_position)
         self.cov = cov
+        self.toroidal_function = toroidal_function
 
-    def move(self, distance=None, toroidal_function=None):
+    def move(self, distance=None):
         self.position = random.multivariate_normal(self.position, self.cov, 1)[0]
-        if toroidal_function:
-            self.position = toroidal_function(self.position)
+        if self.toroidal_function:
+            self.position = self.toroidal_function(self.position)
 
     def __set_init_position(self, init_position):
         if init_position is None:
